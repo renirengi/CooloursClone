d6772181891e94ae53ef472d594dc9916250ecae
Olga <TomieForever@gmail.com>
2022-10-19 15:56:34 +0400

    add change icon

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const cols = document.querySelectorAll ('.col');
 
+document.addEventListener ('keydown', (event) => {
+    event.preventDefault();
+    if (event.code.toLowerCase() == 'space') {
+        setRandomColors(); 
+    }
+});
+document.addEventListener ('click', (event) => {
+  const type = event.target.dataset.type;
+  if (type == 'lock') {
+   const node = event.target.tagName.toLowerCase() == 'i'
+   ? event.target
+   : event.target.children[0];
+
+   node.classList.toggle('fa-lock-open');
+   node.classList.toggle('fa-lock');
+  }
+});
+
 setRandomColors();
 
 function generateRandomColor() {
